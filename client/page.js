6c9c91c725382fbc2d7a9c8f7b7d177ce89de2fc
dev-sda1 <43112896+dev-sda1@users.noreply.github.com>
2021-06-21 02:56:23 +0100

    fuck react and all other javascript frontend frameworks
    title

--- client/page.js
+++ client/page.js
@@ -1,26 +1,34 @@
+//  Make sure you import jquery before using this!
+
+//  Use this on a website that is lucky enough to not use a javascript powered framework 
+//  for a personal-website frontend
+
 $.ajax({
-    url: "https://yoursite.com/api/currentplaying",
-    type: "get",
-    dataType: "JSON",
+    url: 'https://yourweb.site/api/currentplaying',
+    type: 'get',
+    dataType: 'JSON',
     success: function(resp){
-        console.log("DONE!");
+        //console.log("DONE!");
         //console.log(resp['trackname']);
         //console.log(resp['author']);
+        //console.log(resp);
         if(resp['playing'] == true){
-            console.log(resp['url']);
-            console.log(resp['playing']);
+            //console.log(resp['url']);
+            //console.log(resp['playing']);
             $(spotify).removeClass(' hidden');
-            var a = document.getElementById('container2');
-            var b = document.getElementById('container');
+            var a = document.getElementById('spotify');
     
-            a.className += " spotify_add";
-            b.className += " spotify_add";
+            a.className += " show";
             var songName = resp['trackname'];
-			var artists = resp['author'];
+		var artists = resp['author'];
+            var albumTitle = resp['albumtitle'];
+            var albumArtURL = resp['albumart'];
             
-            $(spotifyContext).text("Listening to Spotify: "+songName+" - "+artists);
+            $(songname).text(songName);
+            $(songartists).text("by "+artists);
+            $(album).text("on "+albumTitle)
             $(spotifyURL).attr("href", resp['url']);
-    
+            $(albumart).attr("src", albumArtURL)
     
         }
     }
